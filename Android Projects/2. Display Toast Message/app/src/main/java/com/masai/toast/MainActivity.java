67343f140af3941ccfc8e10b054f6d2274da8744
nitish906 <nk08522015@gmail.com>
2023-03-05 09:45:06 +0530

    Nitish

---
+++ Android Projects/2. Display Toast Message/app/src/main/java/com/masai/toast/MainActivity.java
@@ -0,0 +1,35 @@
+package com.masai.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1=findViewById(R.id.button);
+
+        Button button2= findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Downloding...",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Uploding...", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
